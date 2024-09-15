941c0a2f9fcdc151a0ce0e14ccc9064114488366
urt <urt5002urt@gmail.com>
2024-09-15 17:16:18 +0200

    IEffect pure virtual fix

--- IEffect.h
+++ IEffect.h
@@ -5,21 +5,21 @@
 class IEffect
 {
 public:
-  virtual void setup();						//����������� ���� ��� ����� �������� �������
-  virtual void make_frame();				//��������� ������ �����
+  virtual void setup()=0;						//����������� ���� ��� ����� �������� �������
+  virtual void make_frame()=0;				//��������� ������ �����
 
-  virtual Options* get_options_ptr();
+  virtual Options* get_options_ptr()=0;
 
-  virtual const byte* get_cutoff_order();	//������� ���������� ������� �����������
-  virtual const byte* get_cutoff_imm();		//����� �����������, ��� ������� ������� �� ����������� 
-  virtual byte get_cutoff_order_len();		//������ _cutoff_order
-  virtual byte get_cutoff_imm_len();		//������ _cutoff_imm_len
+  virtual const byte* get_cutoff_order()=0;	//������� ���������� ������� �����������
+  virtual const byte* get_cutoff_imm()=0;		//����� �����������, ��� ������� ������� �� ����������� 
+  virtual byte get_cutoff_order_len()=0;		//������ _cutoff_order
+  virtual byte get_cutoff_imm_len()=0;		//������ _cutoff_imm_len
 
-  virtual int get_preset_count();
-  virtual const String* get_preset_names();
-  virtual void set_preset(int num = 0);			//����������� ��������� �� ���������
+  virtual int get_preset_count()=0;
+  virtual const String* get_preset_names()=0;
+  virtual void set_preset(int num = 0)=0;			//����������� ��������� �� ���������
 
-  virtual String get_effect_name();
+  virtual String get_effect_name()=0;
 };
 
 #endif
